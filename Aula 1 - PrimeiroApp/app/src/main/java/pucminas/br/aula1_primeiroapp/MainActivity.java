484ad0e7477dcdf19e8355e898b3f63d90e76439
Rafael Badaro <rafael.badaro@mrv.com.br>
2019-06-12 19:27:57 -0300

    Adicionando firebase

--- Aula 1 - PrimeiroApp/app/src/main/java/pucminas/br/aula1_primeiroapp/MainActivity.java
+++ Aula 1 - PrimeiroApp/app/src/main/java/pucminas/br/aula1_primeiroapp/MainActivity.java
@@ -1,12 +1,26 @@
 package pucminas.br.aula1_primeiroapp;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    Map<String, Double> user = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +36,27 @@ public class MainActivity extends AppCompatActivity {
         double altura = Double.parseDouble(edtAltura.getText().toString())/100;
         double imc = Math.round(peso/(altura*altura));
         txtIMC.setText(String.valueOf(imc));
+        user.put("peso", peso);
+        user.put("altura", altura);
+        user.put("imc", imc);
+        db.collection("IMC - usuarios")
+                    .add(user)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+
+                            Toast.makeText(getApplicationContext(),
+                                    "Dados salvos com sucesso",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(),
+                                "Erro ao salvar dados",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
